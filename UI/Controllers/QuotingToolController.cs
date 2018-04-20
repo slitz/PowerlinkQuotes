99eb77353adf65e6af3af509d10e6920a5a703c0
Scott Litz <scott.litz@gmail.com>
2018-04-19 18:18:00 -0700

    Added price value to quantity tables; made change to controller to store all product data in the SelectItemList value.

--- UI/Controllers/QuotingToolController.cs
+++ UI/Controllers/QuotingToolController.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccess;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -58,10 +59,11 @@ namespace UI.Controllers
             List<SelectListItem> list = new List<SelectListItem>();
             foreach (product product in _data.GetProductsByCategoryId(productCategory))
             {
+                ItemData itemData = new ItemData () { Description = product.description, OrderCode = product.order_code,  Price = product.price };
                 list.Add(new SelectListItem
                 {
                     Text = String.Format("{0} - {1}", product.order_code, product.description),
-                    Value = product.order_code
+                    Value = JsonConvert.SerializeObject(itemData)
                 });
             }
             return list;
